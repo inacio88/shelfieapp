26ee2944b5cf92d4d51cf61b8ee5959e7cf9f2fc
inacio88 <inacio88@outlook.com>
2025-04-14 15:41:02 -0300

    main: styles

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import Logo from '../assets/img/logo_light.png'
 import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>The number 1</Text>
-      <Text>Reading list app</Text>
+    <View style={styles.container}>
+        <Image source={Logo} style={styles.img} />
+        <Text style={styles.title}>The number 1</Text>
+
+        <Text style={{marginTop: 10, marginBottom: 30}}>
+            Reading list app
+        </Text>
+
+        <View style={styles.card}>
+            <Text>Hello, this is a card.</Text>
+        </View>
+
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    title:
+    {
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+
+    card: {
+        backgroundColor: '#eee',
+        padding: 20,
+        borderRadius: 5,
+        boxShadow: '4px 4px rgba(0,0,0,0.1)'
+    },
+    img: {
+        marginVertical: 20
+    }
+
+})
